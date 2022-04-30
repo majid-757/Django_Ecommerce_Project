c53f8a5b0aae4adaaf404a33c784a597ff2b0a6b
majid-757 <majid.asadollahi95@gmail.com>
2022-04-30 22:36:33 +0430

    config urls in accounts app

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('home.urls', namespace='home')),
-    # path('accounts', include('accounts.urls')),
+    path('accounts', include('accounts.urls')),
 ]
