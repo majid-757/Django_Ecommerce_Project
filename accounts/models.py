3a01e7db3604cf0068bec2a73b07fdd95e6edfc3
majid <majid.asadollahi95@gmail.com>
2022-09-12 23:37:03 +0430

    edit navbar

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,7 @@ class User(AbstractBaseUser):
     full_name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    
 
     objects = UserManager()
 
