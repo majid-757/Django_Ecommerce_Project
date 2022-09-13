d842cd4c88d34933b7cd46daae5cfb04c95cebc7
majid <majid.asadollahi95@gmail.com>
2022-09-13 23:57:05 +0430

    create new model

--- accounts/models.py
+++ accounts/models.py
@@ -35,3 +35,20 @@ class User(AbstractBaseUser):
 
 
 
+
+class OtpCode(models.Model):
+    phone_number = models.CharField(max_length=50)  
+    code = models.PositiveSmallIntegerField()
+    created = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'{self.phone_number} - {self.code} - {self.created}'
+    
+
+
+
+
+
+
+
