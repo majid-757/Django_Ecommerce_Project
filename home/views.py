89da935594978a85d9a7fafe1af0349736b8e398
majid <majid.asadollahi95@gmail.com>
2022-10-06 00:30:13 +0330

    create detail product

--- home/views.py
+++ home/views.py
@@ -18,6 +18,27 @@ class HomeView(View):
 
 class ProductDetailView(View):
     def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'home/detail.html', {'product': product})
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
